878bd5f1a5a10ec482aa412a556dcf20baf6a232
Muhammad <sirmoohfabrics@gmail.com>
2023-03-17 11:53:32 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main- main function
+ * main - main function
  *
  * Return: always 0
  */
@@ -11,7 +11,7 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		printf("%d", i);
-	printf("\n");
+		putchar(i + '0');
+	putchar('\n');
 	return (0);
 }
