2772af357c40d91cd834654ffbe1d0bf65f58ca1
Muhammad <sirmoohfabrics@gmail.com>
2023-03-17 15:22:15 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,10 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				if (i + j != 17)
-				putchar(',');
-				putchar(' ');
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
