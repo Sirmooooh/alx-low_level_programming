b336d4282a0570273264b33958b7e1c5e90c2a15
Muhammad <sirmoohfabrics@gmail.com>
2023-05-04 07:22:34 -0700

    task 1-5

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int k, count = 0;
+	unsigned long int start;
+
+	for (k = 63; k >= 0; k--)
+	{
+		start = n >> k;
+
+		if (start & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
